74975b0cfc5b546a54a16635ff6870531e5e4b2d
Apraxias <castillaagustin6@gmail.com>
2020-12-02 15:11:20 -0500

    Day2 Part2

--- day2/main.cpp
+++ day2/main.cpp
@@ -5,7 +5,7 @@
 using namespace std;
 
 //checker, return if valid
-bool check_pass(string ip)
+bool check_passP1(string ip)
 {
     int iplen = ip.length();
     int index_dash, index_char;
@@ -40,6 +40,34 @@ bool check_pass(string ip)
     return count >= limin && count <= limax;
 }
 
+bool check_passP2(string ip)
+{
+    int iplen = ip.length();
+    int index_dash, index_char;
+
+    for (int i = 0; i < iplen; i++)
+    {
+        if (ip[i] == '-')
+        {
+            index_dash = i;
+        }
+        if (ip[i] == ':')
+        {
+            index_char = i - 1;
+            break;
+        }
+    }
+
+    int first_index = stoi(ip.substr(0, index_dash)) - 1;
+    int second_index = stoi(ip.substr(index_dash + 1, index_char - index_dash - 2)) - 1;
+    char key = ip[index_char];
+    string password = ip.substr(index_char + 3, ip.length() - index_char - 2);
+    int passlen = password.length();
+    int count = 0;
+
+    return password[first_index] == key && password[second_index] != key || password[first_index] != key && password[second_index] == key;
+}
+
 int main(void)
 {
     string buffer;
@@ -50,7 +78,7 @@ int main(void)
     while (!ifp.eof())
     {
         getline(ifp, buffer);
-        if (check_pass(buffer))
+        if (check_passP2(buffer))
         {
             counter++;
         }
